7da38773bb07d0d089f2d76b53502f9ad0bb0499
jordanmefujie <mjordanlonpo@gmail.com>
2024-02-18 13:17:44 -0800

    Files completed.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+#!/usr/bin/python3
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Welcome to the Girlchild API"
+    return "Welcome to the MenstralCycle API"
 
 if __name__ == '__main__':
     app.run(debug=True)
